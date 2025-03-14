0fd02eeb85948a56363ba07751e9c83e4c59887b
Rubens Pinheiro Goncalves Cavalcante <rubenspgcavalcante@gmail.com>
2025-03-14 15:16:54 +0000

    chore: adding webpack compilation

--- dist/index.js
+++
@@ -1,4 +0,0 @@
-on("ready", function () {
-    console.log("Hello Roll20");
-});
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEsRUFBRSxDQUFDLE9BQU8sRUFBRTtJQUNWLE9BQU8sQ0FBQyxHQUFHLENBQUMsY0FBYyxDQUFDLENBQUM7QUFDOUIsQ0FBQyxDQUFDLENBQUMifQ==
